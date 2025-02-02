e6d5b1ca74f8c1e2646939773557223b379d1a3a
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-02-01 22:03:15 -0300

    feat: calculate pace when button clicked

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-const labelResultKmH = document.getElementById('result-km-h');
+const labelResult = document.getElementById('result-label');
+const labelResultDescription = document.getElementById('result__description');
 
-const buttonCalculateKmH = document.getElementById('calculate-kmh').addEventListener('click', ()=>{
+const buttonCalculateKmH = document.getElementById('calculate').addEventListener('click', ()=>{
     //Get input values
     const inputDistance = parseFloat(document.getElementById('total-km').value);
     const inputTotalTime = document.getElementById('total-time').value;
-    
+
     if (!inputDistance || !inputTotalTime){
-        labelResultKmH.textContent = "Please enter both distance and time.";
+        labelResult.textContent = "Please enter both distance and time.";
+        labelResult.style.fontSize = "1rem";
         return;
     }
     
+    //Determine total time in hours
     const [hours, minutes] = inputTotalTime.split(":").map(Number);
     const totalTimeInHours = hours + minutes / 60;
     
-    //Calculate speed in km/h
-    const resultKmPerH = inputDistance / totalTimeInHours;
-    
-    labelResultKmH.innerText = `${resultKmPerH}km/h`;
-    labelResultKmH.style.display = 'flex';
+    switch(getCheckedValue()){
+        case "radio-km-h":
+            //Calculate speed in km/h
+            const resultKmPerH = inputDistance / totalTimeInHours;
+            
+            editResultTexts("You'll need to run:", `${resultKmPerH.toFixed(1)}km/h`);
+            break;
+            
+        case "radio-pace":
+             //Calculate pace per km
+            const totalTimeInSeconds = (hours * 3600) + (minutes * 60);
+            const paceInSeconds = totalTimeInSeconds / inputDistance;
+
+            //Get whole minuts
+            const paceMinutes = Math.floor(paceInSeconds / 60);
+            // Get remaining seconds
+            const paceSeconds = Math.round(paceInSeconds % 60);
+             // Format as mm:ss
+            const resultPace = `${paceMinutes}:${paceSeconds.toString().padStart(2, '0')}`;
+            editResultTexts("Your pace will be:", `${resultPace} min/km`);
+            break;
+    }
 })
 
+function getCheckedValue(){
+    var radios = document.getElementsByName('result-type');
+    for (var i=0; i < radios.length; i++){
+        if (radios[i].checked){
+            return radios[i].id;
+        }
+    }
+}
+
+function editResultTexts(description, result){
+    labelResultDescription.innerText = description;
+    labelResult.innerText = result;
+
+    // labelResult.style.fontSize = "";
+    // labelResult.style.display = 'flex';
+    return;
+}
