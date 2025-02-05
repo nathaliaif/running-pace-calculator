6ccb59d4a814b23b057bf0be87a41dd53ea4c5ce
nathaliaif <nathalia.iro.fernandes@gmail.com>
2025-02-05 09:55:32 -0300

    feat: icon on tab

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const labelResult = document.getElementById('result-label');
 const labelResultDescription = document.getElementById('result__description');
 const resetButton = document.getElementById('reset');
+const radios = document.getElementsByName('result-type');
+
+const RESULT_DESCRIPTION_TEXTS = {
+    KM_H: "You'll need to run:",
+    PACE: "Your pace will be:"
+}
+
+for(var i=0; i < radios.length; i++){
+    radios[i].addEventListener("click", () => {
+        switch(getCheckedValue()){
+            case "radio-km-h":
+                labelResultDescription.innerText = RESULT_DESCRIPTION_TEXTS.KM_H;
+                break;
+            case "radio-pace":
+                labelResultDescription.innerText = RESULT_DESCRIPTION_TEXTS.PACE;
+                break;
+        }
+    })
+}
 
 const buttonCalculateKmH = document.getElementById('calculate').addEventListener('click', ()=>{
     //Get input values
     const inputDistance = parseFloat(document.getElementById('total-km').value);
+    
     const inputTotalTime = document.getElementById('total-time').value;
+    labelResult.style.fontSize = "1.5rem";
 
     if (!inputDistance || !inputTotalTime){
         labelResult.textContent = "Please enter both distance and time.";
@@ -22,7 +43,7 @@ const buttonCalculateKmH = document.getElementById('calculate').addEventListener
             //Calculate speed in km/h
             const resultKmPerH = inputDistance / totalTimeInHours;
             
-            editResultTexts("You'll need to run:", `${resultKmPerH.toFixed(1)}km/h`);
+            editResultTexts(RESULT_DESCRIPTION_TEXTS.KM_H, `${resultKmPerH.toFixed(1)}km/h`);
             break;
             
         case "radio-pace":
@@ -36,7 +57,7 @@ const buttonCalculateKmH = document.getElementById('calculate').addEventListener
             const paceSeconds = Math.round(paceInSeconds % 60);
              // Format as mm:ss
             const resultPace = `${paceMinutes}:${paceSeconds.toString().padStart(2, '0')}`;
-            editResultTexts("Your pace will be:", `${resultPace} min/km`);
+            editResultTexts(RESULT_DESCRIPTION_TEXTS.PACE, `${resultPace} min/km`);
             break;
     }
 })
@@ -46,12 +67,11 @@ resetButton.addEventListener('click', ()=>{
     inputs.forEach(input => {
         input.type == "time" ?  input.value = "00:00" : input.value = '';
     })
-
-    editResultTexts("You'll need to run:", '--');
+    radios[0].checked = true;
+    editResultTexts(RESULT_DESCRIPTION_TEXTS.KM_H, '--');
 })
 
 function getCheckedValue(){
-    var radios = document.getElementsByName('result-type');
     for (var i=0; i < radios.length; i++){
         if (radios[i].checked){
             return radios[i].id;
@@ -62,8 +82,5 @@ function getCheckedValue(){
 function editResultTexts(description, result){
     labelResultDescription.innerText = description;
     labelResult.innerText = result;
-
-    // labelResult.style.fontSize = "";
-    // labelResult.style.display = 'flex';
     return;
 }
